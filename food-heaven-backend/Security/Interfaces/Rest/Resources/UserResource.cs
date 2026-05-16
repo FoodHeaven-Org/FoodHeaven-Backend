c139b4c9724dda3e057d3dba7b7e1a728f12e547
luis <luispierorodriguez@gmail.com>
2026-05-16 07:16:39 -0500

    fix: hide user password hashes from api

---
+++ food-heaven-backend/Security/Interfaces/Rest/Resources/UserResource.cs
@@ -0,0 +1,23 @@
+using food_heaven_backend.Security.Domain.Model.Entities;
+
+namespace food_heaven_backend.Security.Interfaces.Rest.Resources;
+
+public record UserResource(
+    int Id,
+    string Username,
+    string Subscription,
+    int Phone,
+    string City
+)
+{
+    public static UserResource FromEntity(User user)
+    {
+        return new UserResource(
+            user.Id,
+            user.Username,
+            user.Subscription,
+            user.Phone,
+            user.City
+        );
+    }
+}
