2c353849ea04cab9e2b76497dca86ce34b34b8a8
luis <luispierorodriguez@gmail.com>
2026-05-16 02:28:21 -0500

    refactor: validate security domain messages

--- food-heaven-backend/Security/Domain/Model/Commands/LoginCommand.cs
+++ food-heaven-backend/Security/Domain/Model/Commands/LoginCommand.cs
@@ -1,3 +1,16 @@
 namespace food_heaven_backend.Security.Domain.Model.Commands;
 
-public record LoginCommand(String Username, String Password);
+public record LoginCommand
+{
+    public string Username { get; init; }
+    public string Password { get; init; }
+
+    public LoginCommand(string username, string password)
+    {
+        if (string.IsNullOrWhiteSpace(username)) throw new ArgumentException("Username is required.", nameof(username));
+        if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Password is required.", nameof(password));
+
+        Username = username;
+        Password = password;
+    }
+}
--- food-heaven-backend/Security/Domain/Model/Queries/GetUserByIdQuery.cs
+++ food-heaven-backend/Security/Domain/Model/Queries/GetUserByIdQuery.cs
@@ -2,10 +2,11 @@ namespace food_heaven_backend.Security.Domain.Model.Queries;
 
 public record GetUserByIdQuery
 {
+    public int UserId { get; init; }
+
     public GetUserByIdQuery(int userId)
     {
+        if (userId <= 0) throw new ArgumentException("UserId must be greater than zero.", nameof(userId));
         UserId = userId;
     }
-
-    public int UserId { get; init; }
 }
